953a50a2034e6b5dd3f9990aaaa46f85cb071335
nicko <37054216+nicko-lee@users.noreply.github.com>
2021-07-02 17:01:34 +1000

    Initial commit to setup frame of weather app for web

---
+++ src/app.js
@@ -0,0 +1,72 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+// Create express server
+const app = express()
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static dir to server
+app.use(express.static(publicDirectoryPath))
+
+
+// Specify routes/handlers
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Sunny Baudelaire'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Sunny Baudelaire'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        helpMsg: 'Please help yourself to the cake.',
+        name: 'Sunny Baudelaire'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    res.send({
+        location: 'Philadelphia',
+        forecast: 'It is currently 14 degrees. It feels like 16 degrees!',
+        name: 'Sunny Baudelaire'
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        name: 'Sunny Baudelaire',
+        errorMessage: 'Help article not found.',
+        title: 404
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        name: 'Sunny Baudelaire',
+        errorMessage: 'Page not found.',
+        title: '404'
+    })
+})
+
+// Start express server
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
